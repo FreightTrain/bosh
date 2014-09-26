f72d1ea58c7e0904ef1fbfdd127f3f1b99d21b78
Matt Johnson <matjohn2@cisco.com>
2014-09-26 18:00:28 +0100

    bad save for new apply.sh

--- stemcell_builder/stages/system_growroot/apply.sh
+++ stemcell_builder/stages/system_growroot/apply.sh
@@ -6,18 +6,13 @@ base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
 source $base_dir/lib/prelude_bosh.bash
 
-pkg_mgr install wireless-crda
-
-mkdir -p $chroot/tmp
 
 if [ "${DISTRIB_CODENAME}" == "lucid" ]; then
   cp $assets_dir/lucid/*.deb $chroot/tmp/
 
-  run_in_chroot $chroot "dpkg -i /tmp/linux-headers-3.0.0-32_3.0.0-32.51~lucid1_all.deb"
-  run_in_chroot $chroot "dpkg -i /tmp/linux-headers-3.0.0-32-virtual_3.0.0-32.51~lucid1_amd64.deb"
-  run_in_chroot $chroot "dpkg -i /tmp/linux-image-3.0.0-32-virtual_3.0.0-32.51~lucid1_amd64.deb"
+  run_in_chroot $chroot "dpkg -i /tmp/lucid-growroot_0.1ubuntu1~ppa0_all.deb"
 
   rm $chroot/tmp/*.deb
 else
-  pkg_mgr install linux-generic
+  echo "No installation candidate on this arch for growroot"
 fi
